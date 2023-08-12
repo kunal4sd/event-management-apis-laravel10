d0024c0ceacf28f8286b34f410b79e5d73294ce7
Kunal Sachdeva <kunal4sd@gmail.com>
2023-08-12 17:32:25 +0600

    update event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -45,9 +45,17 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update(
+            $request->validate([
+                'name'=>'sometimes|string|max:255',
+                'description'=>'nullable|string',
+                'start_time'=>'sometimes|date',
+                'end_time'=>'sometimes|date|after:start_time'
+        ]));
+
+        return $event;
     }
 
     /**
